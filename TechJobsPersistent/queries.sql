1a7fe80b27e855b2afc0ae5441e35931b631cc5d
KristineGray <krisgraya@gmail.com>
2021-08-10 01:34:26 -0400

    completed SQL Task Part 3 in queries.sql

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -2,7 +2,10 @@
 -- Id (int), Name (longtext), and EmployerId (int)
 
 --Part 2
-SELECT name FROM employers WHERE location = "St.Louis City";
+SELECT name FROM employers WHERE location = "Saint Louis";
 
 --Part 3
-
+SELECT s.name, s.description FROM jobs j
+INNER JOIN jobskills js ON j.Id = js.JobId
+INNER JOIN skills s ON js.SkillId = s.Id
+ORDER BY Name, Description ASC;
